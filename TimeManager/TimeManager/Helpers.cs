528908cde72f8c4eed6a05cfddf43b1e44fe07ae
michalOch <ochrobot1990@gmail.com>
2020-12-01 19:13:36 +0100

    add static helpers class with three methods for retrieving data from the user

---
+++ TimeManager/TimeManager/Helpers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeManager
+{
+    public static class Helpers
+    {
+        public static string GetStringValue(string message)
+        {
+            string value = string.Empty;
+            do
+            {
+                Console.WriteLine(message);
+                value = Console.ReadLine();
+
+            } while (string.IsNullOrWhiteSpace(value));
+
+            return value;
+        }
+
+        public static double GetDoubleNumber(string message)
+        {
+            double value = 0.0;
+            do
+            {
+                GetStringValue(message);
+                double.TryParse(message, out value);
+
+            } while (value <= 0);
+            return value;
+        }
+
+        public static double GetIntNumber(string message)
+        {
+            int value = 0;
+            do
+            {
+                GetStringValue(message);
+                int.TryParse(message, out value);
+            } while (value <= 0);
+            return value;
+        }
+    }
+}
