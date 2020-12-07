118f77493d3cd4e1c59ad707dad151a7c7d9977d
michalOch <ochrobot1990@gmail.com>
2020-12-07 07:13:24 +0100

    move task  mock data from TaskService to MockTask

--- TimeManager/TimeManager/TaskService.cs
+++ TimeManager/TimeManager/TaskService.cs
@@ -6,12 +6,15 @@ namespace TimeManager
 {
     public class TaskService
     {
+        private MockTask mockTask;
+
         public static int counter = 10;
         public List<Task> Tasks { get; set; }
 
         public TaskService()
         {
             Tasks = new List<Task>();
+            mockTask = new MockTask();
             CreateMockData();
         }
 
@@ -89,89 +92,7 @@ namespace TimeManager
 
         public void CreateMockData()
         {
-            Tasks = new List<Task>()
-            {
-                new Task()
-                {
-                    Id = 1,
-                    Name = "Design pedestal",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-                    EstimatedHours = 10.0d
-                },
-
-                new Task()
-                {
-                    Id = 2,
-                    Name = "Design gripper",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-                    EstimatedHours = 20.0d
-                },
-
-                new Task()
-                {
-                    Id = 3,
-                    Name = "Design conveyor",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-                    EstimatedHours = 30.0d
-                },
-
-                new Task()
-                {
-                    Id = 4,
-                    Name = "Design safety fence",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-                    EstimatedHours = 10.0d
-                },
-
-                new Task()
-                {
-                    Id = 5,
-                    Name = "Design control panel",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-                    EstimatedHours = 40.0d
-                },
-
-                new Task()
-                {
-                    Id = 6,
-                    Name = "Create electric diagrams",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-                    EstimatedHours = 15.5d
-                },
-
-                new Task()
-                {
-                    Id = 7,
-                    Name = "Design safety system",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-                    EstimatedHours = 23.0d
-                },
-
-                new Task()
-                {
-                    Id = 8,
-                    Name = "Create 3D model",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-                    EstimatedHours = 10.0d
-                },
-
-                new Task()
-                {
-                    Id = 9,
-                    Name = "Create simulation",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-                    EstimatedHours = 25.5d
-                },
-
-                new Task()
-                {
-                    Id = 10,
-                    Name = "assembly of the robot at the customer's",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-                    EstimatedHours = 5.0d
-                },
-
-            };
+            Tasks = mockTask.GetMockData();
         }
     }
 }
